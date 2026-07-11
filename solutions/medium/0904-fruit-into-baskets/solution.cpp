b714f01454f37769ffcaa4e657c66af99061377e
muizz287 <mmdmuizzahmed.09.a@gmail.com>
2026-07-11 17:52:20 +0530

    Add 904. Fruit Into Baskets [Medium] - 39 ms / 85 MB

---
+++ solutions/medium/0904-fruit-into-baskets/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int totalFruit(vector<int>& fruits) {
+        // this problem is about finding a max subarray
+        // which has atmost two distinct elements.
+
+        int n = fruits.size();
+        int left = 0;
+        int right = 0;
+        int maxFruits = 0;
+        unordered_map<int, int> freq;
+        while(right < n){
+            freq[fruits[right]]++;
+            while(freq.size() > 2){
+                freq[fruits[left]]--;
+                if(freq[fruits[left]] == 0) freq.erase(fruits[left]);
+                left++;
+            }
+            int len = right - left + 1;
+            maxFruits = max(maxFruits, len);
+            right++;
+        }
+        return maxFruits;
+    }
+};

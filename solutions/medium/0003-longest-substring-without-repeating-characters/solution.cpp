f84d2c72d453a2cd8a330f82fc76b9e1807f0b0e
muizz287 <mmdmuizzahmed.09.a@gmail.com>
2026-07-11 14:41:36 +0530

    Add 3. Longest Substring Without Repeating Characters [Medium] - 0 ms / 11.7 MB

--- solutions/medium/0003-longest-substring-without-repeating-characters/solution.cpp
+++ solutions/medium/0003-longest-substring-without-repeating-characters/solution.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
         int l = s.length();
+        vector<int> hashmap(255, -1);
+        int left = 0;
+        int right = 0;
         int maxLen = 0;
-        for(int i=0;i<l;i++){
-            vector<int> mp(255, 0);
-            for(int j=i;j<l;j++){
-                if(mp[s[j]] == 1) break;
-                int len = j - i + 1;
-                maxLen = max(maxLen, len);
-                mp[s[j]] = 1;
+        while(right < l){
+            if(hashmap[s[right]] != -1){
+                if(hashmap[s[right]] >= left){
+                    left = hashmap[s[right]] + 1;
+                }
             }
+            int len = right - left + 1;
+            maxLen = max(maxLen, len);
+            hashmap[s[right]] = right;
+            right++;
         }
 
         return maxLen;

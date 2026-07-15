a2d9ffec0e07cb524cad4e9d59616ce83060734b
muizz287 <mmdmuizzahmed.09.a@gmail.com>
2026-07-15 14:29:59 +0530

    Add 3658. GCD of Odd and Even Sums [Easy] - 0 ms / 8.5 MB

--- solutions/easy/3658-gcd-of-odd-and-even-sums/solution.cpp
+++ solutions/easy/3658-gcd-of-odd-and-even-sums/solution.cpp
@@ -4,34 +4,8 @@ public:
         if(b == 0) return a;
         return gcd(b, a % b);
     }  
-    
+
     int gcdOfOddEvenSums(int n) {
-        int oddSum = 0;
-        int evenSum = 0;
-        int num = n;
-
-        int oddNum = 1;
-        while(num != 0){
-            if(oddNum % 2 != 0){
-                oddSum += oddNum;
-                oddNum = oddNum + 2;
-                num--;
-            }
-        }
-
-        num = n;
-        int evenNum = 2;
-        while(num != 0){
-            if(evenNum % 2 == 0){
-                evenSum = evenSum + evenNum;
-                evenNum = evenNum + 2;
-                num--;
-            }
-        }
-
-        cout << "odd sum : " << oddSum << endl;
-        cout <<"even sum : " << evenSum << endl;
-
-        return gcd(evenSum, oddSum);
+        return gcd(n*n, n*(n+1));
     }
 };
